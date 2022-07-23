63a2621bfe3e964c1080f45c2f939df7cf44884c
theCoderSchool-CoralSprings <rajiv@thecoderschool.com>
2022-07-23 13:04:28 -0400

    colorkey

--- pacman.py
+++ pacman.py
@@ -6,15 +6,23 @@ pygame.init()
 screen = pygame.display.set_mode((400,400))
 clock = pygame.time.Clock()
 board = pygame.image.load("pacman grid.png")
-board = pygame.transform.scale(board,(400,400))
+board.set_colorkey((0,0,0))
+board = pygame.transform.scale(board,(1100,1100))
+
+
+print(board.get_alpha(),board.get_colorkey())
 board_Rect = board.get_rect()
 board_mask = pygame.mask.from_threshold(board, (0,255,0))
 pacman = pygame.image.load("pacman.jpg")
-pacman = pygame.transform.scale(pacman,(30,30))
+#pacman = pygame.transform.scale(pacman,(30,30))
+pacman.set_colorkey((255,255,255))
+
 pacman_Rect = pacman.get_rect()
 redghost = pygame.image.load("redghost.png")
+redghost.set_colorkey((255,255,255))
 redghost = pygame.transform.scale(redghost,(30,30))
 redghost_Rect = redghost.get_rect()
+
 
 class pacman:
     def __init__(self, screen, board, board_Rect, board_mask) -> None:
@@ -27,12 +35,14 @@ class pacman:
         self.direction = 0
         self.screen = screen
         self.board, self.board_Rect, self.board_mask = board, board_Rect, board_mask
+
         
     
 
 
     def draw(self):
         self.screen.blit(self.pacman, self.pacman_Rect)
+
 
 
     def move(self):
@@ -50,6 +60,8 @@ class pacman:
             self.pacman_Rect.x -= 2
         elif self.direction == 270:
             self.pacman_Rect.y += 2
+        if pacman_Rect.colliderect(board_Rect):
+            print("collision")
 
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_w]:
@@ -81,7 +93,7 @@ while True:
 
 
         #game code
-
+    screen.fill((0,0,0))
     screen.blit(board, board_Rect)
     screen.blit(redghost, redghost_Rect)
     pacman1.move()
